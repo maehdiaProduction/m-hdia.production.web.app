4508c967d56e7aeb53a7b7799f61739d1c39d99a
Maehdia <maahdiaproductions@gmail.com>
2023-05-31 00:58:28 +0200

    scroll to top button added

--- js/home.js
+++ js/home.js
@@ -5,6 +5,20 @@
  *   var Webflow = Webflow || [];
  *   Webflow.push(readyFunction);
  */
+var btn = $("#button");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
 
 (() => {
   var __getOwnPropNames = Object.getOwnPropertyNames;
